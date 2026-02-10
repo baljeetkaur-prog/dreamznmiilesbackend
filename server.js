659de2b18a5afcba6c0a0e92d5822356d6069ebd
baljeetkaur-prog <baljeetkor6@gmail.com>
2026-02-10 17:14:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -78,10 +78,7 @@ app.use(express.json());
 
 
 // ===== MongoDB Connection =====
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.error("DB Connection Error:", err));
 
